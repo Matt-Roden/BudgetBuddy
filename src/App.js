e5efb2cbd3398422abd7a0ea9b2b3d180c17eaf4
Matt Roden <mattroden92@gmail.com>
2021-09-10 19:52:47 -0600

    Begin building out App methods - not all comps built yet

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react'
+
 import './App.css';
 
 import Header from '../Header/Header'
@@ -5,12 +7,49 @@ import IncomeForm from '../IncomeForm/IncomeForm'
 import ExpenseForm from '../ExpenseForm/ExpenseForm'
 import BudgetDisplay from '../BudgetDisplay/BudgetDisplay'
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      income: 0,
+      expenses: [],
+      totalExpenses: 0,
+      totalSavings: 0
+    }
+  }
+
+  addIncome = (income) => {
+    this.setState({ income: income })
+  }
+
+  addNewExpense = (expense) => {
+    this.setState({ expenses: [...this.state.expenses, expense] })
+  }
+
+  getTotalExpenses = () => {
+    const total = this.state.expenses.reduce((acc, expense) => {
+      acc += expense.amount
+      return acc
+    }, 0)
+
+    this.setState({
+      totalExpenses: total
+    })
+  }
+
+  getTotalSavings = () => {
+    const totalSavings = (this.state.income) - (this.state.totalExpenses)
+    
+    this.setState({ totalSavings: totalSavings })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <IncomeForm />
+      </div>
+    )
+  }
 }
 
 export default App;
