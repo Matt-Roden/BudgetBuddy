9831448bef3d3dae77c3c09ff114621e8e11f19b
Matt Roden <mattroden92@gmail.com>
2021-09-17 23:05:09 -0600

    Refactor to hooks - intial

--- src/Components/IncomeForm/IncomeForm.js
+++ src/Components/IncomeForm/IncomeForm.js
@@ -1,48 +1,93 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './IncomeForm.css'
 
-class IncomeForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      amount: 0,
-    }
+const IncomeForm = ({ addIncome }) => {
+  const [amount, setAmount] = useState(0)
+
+  const handleInputChange = (event) => {
+    setAmount(event.target.value)
   }
 
-  handleInputChange = (event) => {
-    this.setState({amount: event.target.value})
-  }
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
     const income = {
-      amount: parseInt(this.state.amount)
+      amount: parseInt(amount)
     }
-    this.props.addIncome(income)
-    this.resetState()
+
+    addincome(income)
+    resetState()
   }
 
-  resetState = () => {
-    this.setState({
-      amount: 0
-    })
+  const resetState = () => {
+    setAmount(0)
   }
 
-  render() {
-    return (
-      <form className='income-form'>
-        <label htmlFor='amount'>Monthly Income</label>
-        <input
-          className='income-input'
-          type='number'
-          name='amount'
-          value={this.state.amount}
-          onChange={event => this.handleInputChange(event)}
-        />
-        <button onClick={event => this.handleSubmit(event)} className='income-button'>Submit</button>
-      </form>
-    )
-  }
+  return (
+    <form className='income-form'>
+      <label htmlFor='amount'>Monthly Income</label>
+      <input
+        className='income-input'
+        type='number'
+        name='amount'
+        value={this.state.amount}
+        onChange={event => this.handleInputChange(event)}
+      />
+      <button onClick={event => this.handleSubmit(event)} className='income-button'>Submit</button>
+    </form>
+  )
 }
 
 export default IncomeForm
+
+
+
+
+
+
+
+// class IncomeForm extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       amount: 0,
+//     }
+//   }
+//
+//   handleInputChange = (event) => {
+//     this.setState({amount: event.target.value})
+//   }
+//
+//   handleSubmit = (event) => {
+//     event.preventDefault()
+//     const income = {
+//       amount: parseInt(this.state.amount)
+//     }
+//     this.props.addIncome(income)
+//     this.resetState()
+//   }
+//
+//   resetState = () => {
+//     this.setState({
+//       amount: 0
+//     })
+//   }
+//
+//   render() {
+//     return (
+//       <form className='income-form'>
+//         <label htmlFor='amount'>Monthly Income</label>
+//         <input
+//           className='income-input'
+//           type='number'
+//           name='amount'
+//           value={this.state.amount}
+//           onChange={event => this.handleInputChange(event)}
+//         />
+//         <button onClick={event => this.handleSubmit(event)} className='income-button'>Submit</button>
+//       </form>
+//     )
+//   }
+// }
+//
+// export default IncomeForm
